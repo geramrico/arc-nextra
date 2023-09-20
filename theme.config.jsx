3b9c2191fab28fd84b1276af8ad3a3d52c837d79
Gerardo <geramoralesrico@gmail.com>
2023-09-19 23:46:56 -0600

    first commit

---
+++ theme.config.jsx
@@ -0,0 +1,25 @@
+export default {
+  logo: <span>Demo Cash</span>,
+  project: {
+    link: 'https://arcusfi.com',
+    icon: '',
+  },
+  feedback: {
+    content: null,
+  },
+  editLink: {
+    text: null,
+  },
+  footer: {
+    text: (
+      <span>
+        {new Date().getFullYear()} ©{' '}
+        <a href="arcusfi.com" target="_blank">
+          Demo Docs GMR
+        </a>
+        .
+      </span>
+    ),
+  },
+  // ...
+}
